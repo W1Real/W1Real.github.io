9687d8e6c21efbf5e88917a87d5e89205febe49b
Lucas Santos <lucasxp32@users.noreply.github.com>
2020-10-09 01:59:23 -0300

    v4

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function addNumber() {
     number = number + 1;
     document.getElementById("number").innerHTML = number;
 }
+
+function whoIsTheNicestGirl() {
+    var girl = document.getElementById("girl").value;
+    if (girl.toLowerCase() == "cons") {
+        document.getElementById("big-emoji").innerHTML = "😍";
+        document.getElementById("score").innerHTML = "⭐⭐⭐⭐⭐";
+        document.getElementById("score").classList.add('score-happy');
+    }
+    else {
+        document.getElementById("big-emoji").innerHTML = "😭";
+        document.getElementById("score").innerHTML = "⭐";
+        document.getElementById("score").classList.remove('score-happy');
+        document.getElementById("score").classList.add('score-sad');
+    }
+
+}
